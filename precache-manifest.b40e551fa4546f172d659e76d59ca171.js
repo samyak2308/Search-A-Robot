e72f6491e57b1a22ec088752cbd28d6151267422
samyak2308 <67095599+samyak2308@users.noreply.github.com>
2020-07-28 18:43:54 +0530

    Updates

---
+++ precache-manifest.b40e551fa4546f172d659e76d59ca171.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680e2c8399ac1e7929e877efccd5f496",
+    "url": "/Search-A-Robot/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Search-A-Robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1c2deb4b1d5f438b0506",
+    "url": "/Search-A-Robot/static/css/main.bb12b3bb.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Search-A-Robot/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-A-Robot/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2deb4b1d5f438b0506",
+    "url": "/Search-A-Robot/static/js/main.cf0a7c02.chunk.js"
+  },
+  {
+    "revision": "2b8b7791e8ba03185b4a",
+    "url": "/Search-A-Robot/static/js/runtime-main.819d8603.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Search-A-Robot/static/media/SEGA.82e09f78.TTF"
+  }
+]);
